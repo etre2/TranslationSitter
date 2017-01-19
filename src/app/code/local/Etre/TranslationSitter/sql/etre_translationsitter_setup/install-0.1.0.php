074545f0ecb01cdc37d1e1a88a690f4e3b4c65e2
Tyler Mills <tmills@monin.com>
2017-01-19 13:36:16 -0500

    Fix setup script

--- src/app/code/local/Etre/TranslationSitter/sql/etre_translationsitter_setup/install-0.1.0.php
+++ src/app/code/local/Etre/TranslationSitter/sql/etre_translationsitter_setup/install-0.1.0.php
@@ -14,19 +14,12 @@ $installerConnection = $installer->getConnection();
 
 $installerConnection
     ->addColumn($coreTranslateTable, 'is_from_translationsitter', [
-        'type'     => Varien_Db_Ddl_Table::TYPE_BOOLEAN,
+        'type' => Varien_Db_Ddl_Table::TYPE_BOOLEAN,
         'nullable' => false,
-        'default'  => 0,
-        'length'   => 1,
-        'after'    => null, // column name to insert new column after
-        'comment'  => 'Translation provided by Etre_TranslationSitter',
-    ])
-    ->addColumn($coreTranslateTable, 'source', [
-        'type'=> Varien_Db_Ddl_Table::TYPE_TEXT,
-        'default'   => "Magento",
-        'nullable'=>false,
-        'after'=>"is_from_translationsitter",
-        'comment'=>''
+        'default' => 0,
+        'length' => 1,
+        'after' => null, // column name to insert new column after
+        'comment' => 'Translation provided by Etre_TranslationSitter',
     ]);
 
 
